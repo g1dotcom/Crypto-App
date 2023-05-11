be607f5c078b7f24091a62829dbf6ffd4cefacce
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-11 21:46:26 +0300

    home.jsx üzerinde değişiklilkler

---
+++ src/components/BodyComp/BodyComp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Bitcoin from "../btc/Bitcoin";
+
+const BodyComp = () => {
+  return (
+    <div className=" w-full h-screen flex justify-center items-center">
+      <Bitcoin />
+    </div>
+  );
+};
+
+export default BodyComp;
