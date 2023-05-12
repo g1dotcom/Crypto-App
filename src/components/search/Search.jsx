c74453af3f8bcc6f3e50392bb81e92a405c94c73
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-12 18:47:40 +0300

    search işlemleri

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -22,12 +22,40 @@ const Search = () => {
     }
   };
   return (
-    <div className="bg-gray-500">
-      <form className="bg-gray-200" onSubmit={handleFormSubmit}>
-        <input type="text" value={coin} onChange={handleInputChange} />
-        <button type="submit">Get Price</button>
+    <div className="w-full flex flex-col items-center justify-center ">
+      <form className="shadow-2xl shadow-gray-500" onSubmit={handleFormSubmit}>
+        <input
+          className="p-2 outline-none  bg-gray-100"
+          type="text"
+          value={coin}
+          onChange={handleInputChange}
+        />
+        <button
+          className="p-2 outline-none hover:bg-gray-300 bg-gray-200"
+          type="submit"
+        >
+          Get Price
+        </button>
       </form>
-      {price && <p>{`${coin.toUpperCase()} price is ${price}`}</p>}
+      <div className=" bg-gray-50 p-2 flex  justify-center items-center shadow-2xl my-6 shadow-gray-500">
+        <div className="mx-2 my-4 ">
+          <img
+            className="w-8"
+            src="https://cryptologos.cc/logos/tether-usdt-logo.png"
+            alt=""
+          />
+        </div>
+        <div className="">
+          <h1 className="">
+            {price && (
+              <p className="text-lg font-bold text-center">
+                {`${coin.toUpperCase()} ${price.slice(0, 6)}`}{" "}
+                <span className="text-xs text-yellow-500">USDT</span>{" "}
+              </p>
+            )}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
