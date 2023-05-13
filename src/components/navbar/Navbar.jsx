cc110299546f8e71a98b13a9e6ac9ced2019cb8d
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-13 16:19:32 +0300

    navbar css design1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-300 rounded-lg">
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="navbar bg-slate-500 rounded-lg">
+      <a className="btn btn-ghost normal-case text-xl bg-white">
+        <span className="text-yellow-500">Goko</span>
+        <span className="bg-yellow-500  text-white">Swap</span>
+      </a>
     </div>
   );
 };
